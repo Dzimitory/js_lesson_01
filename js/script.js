8ea776a8e32c71b1d5fa3f757618da66df51597a
Dima <meister32@mail.ru>
2022-08-29 15:53:20 +0200

    git_lesson_commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+let storeName = "Strela";
+let storeDescription = {
+   budget: 10000,
+   employees: ['Joe','John','Joel'],
+   products: {
+    d: 2323,
+    dsf: 2323
+   }
+};
+
+console.log(storeDescription.employees);
